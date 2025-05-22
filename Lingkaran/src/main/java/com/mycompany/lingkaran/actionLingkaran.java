f1ef4ae6bead9807777a3920f0da36a60f52629a
Kyky-Dev <pungky.prameswari@gmail.com>
2025-05-22 21:18:57 +0700

    Update actionLingkaran.java

--- Lingkaran/src/main/java/com/mycompany/lingkaran/actionLingkaran.java
+++ Lingkaran/src/main/java/com/mycompany/lingkaran/actionLingkaran.java
@@ -5,11 +5,11 @@ public class actionLingkaran {
     public static void main(String[] args) {
         Lingkaran lingkaran = new Lingkaran(11.78);
 
-        System.out.println("Jari-jari lingkaran             : " + lingkaran.getJariJari());
-        System.out.println("Pi                              : " + lingkaran.getPi());
-        System.out.println("Hasilnya adalah (pecahan)       : " + lingkaran.hitungLuas());
-        System.out.println("Hasilnya adalah (dibulatkan)    : " + lingkaran.hitungLuasDibulatkan());
-        System.out.println("Hasilnya adalah (bilangan bulat): " + lingkaran.hitungLuasInteger());
+        System.out.println("Jari-jari lingkaran      : " + lingkaran.getJariJari());
+        System.out.println("Pi                       : " + lingkaran.getPi());
+        System.out.println("Hasilnya (pecahan)       : " + lingkaran.hitungLuas());
+        System.out.println("Hasilnya (dibulatkan)    : " + lingkaran.hitungLuasDibulatkan());
+        System.out.println("Hasilnya (bilangan bulat): " + lingkaran.hitungLuasInteger());
     }
     
 }
